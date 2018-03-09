3e4a753c5c4e07e975575eaf64e58fb41556e1a3
montse <montse94@gmail.com>
2018-03-09 10:07:58 -0600

    Agregando los nuevos paquetes con sus clases

---
+++ pt1.pt2/src/main/java/resources/resPersonal.java
@@ -0,0 +1,24 @@
+package resources;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import classes.Personal;
+
+@Path("/personal")
+public class resPersonal {
+	
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/getLogin")
+	public String getLoginSession(@QueryParam("user") String user, @QueryParam("pass") String pass) {
+    		System.out.println("hola"+user+pass);
+    		Personal personal = new Personal();
+    		return personal.iniciarSesion(user,pass);
+	}
+}
