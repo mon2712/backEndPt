f18824a454039e074f7eb7a45998e290080e81e3
montse <montse94@gmail.com>
2018-04-07 22:43:35 -0500

    Los cambios del back para la ficha del alumno y del buscador

--- pt1.pt2/src/main/java/classes/pruebafecha.java
+++ pt1.pt2/src/main/java/classes/pruebafecha.java
@@ -1,16 +1,25 @@
 package classes;
 
-import classes.Centro;
+//import classes.Centro;
 //import classes.BaseDatos;
 
 public class pruebafecha {
 	public static void main(String[] args) {
 		
 		BaseDatos dataDB = new BaseDatos();
-		Centro centro= new Centro();
+		//Centro centro= new Centro();
 		
-		String Students=centro.getStudents("JOSE");
-		System.out.print(Students);
+		//String Students=centro.getStudents("JOSE");
+		//System.out.print(Students);
+		
+		/*Alumno alumno = new Alumno();
+		String Alumnos=alumno.obtenerFichaAlumno(3);
+		System.out.println(Alumnos);*/
+		
+		Alumno alumno = new Alumno();
+		String alumnitos=alumno.getAlumnos("je");
+		System.out.println(alumnitos);
+		
 	}
 
 }
