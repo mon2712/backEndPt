b20501f03553f643ea376ac7ab1a5a310422be18
VanessaMiranda <VanessaMiranda@192.168.1.65>
2018-07-14 19:55:37 -0500

    funcion de todos los dias, colores, y dias de centro despues de las 7pn

--- pt1.pt2/src/main/java/classes/Recepcion.java
+++ pt1.pt2/src/main/java/classes/Recepcion.java
@@ -8,9 +8,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.json.Json;
@@ -30,7 +32,8 @@ public class Recepcion {
 			CallableStatement cStmt = conn.prepareCall("{call listaNuevasLlamadas(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)}");
 			String getQueryStatement2 = "call getLlamadasEnNotificaciones();";
 
-		    prepareStat = conn.prepareStatement(getQueryStatement);
+		    //Otiene alumnos con una falta
+			prepareStat = conn.prepareStatement(getQueryStatement);
             ResultSet rs = prepareStat.executeQuery(); //devuelbe nombre, apellido, idAlumno, lunes, miercoles, jueves, sabado
             
             //Obteniendo las notificaciones de llamadas viejas
@@ -42,134 +45,157 @@ public class Recepcion {
             int estatus=0, activacion=0; 
             int[] arrayDays = new int[4];
             String old="";
-                 
+
+            //Fecha actual desglosada://///////////////////////////////////////
+            Calendar fecha = Calendar.getInstance();
+            int año = fecha.get(Calendar.YEAR);
+            int mes = fecha.get(Calendar.MONTH) + 1;
+            int dia = fecha.get(Calendar.DAY_OF_MONTH);
+            int hora = fecha.get(Calendar.HOUR_OF_DAY);
+            
+            String hoy= dia + "/" + (mes) + "/" + año;
+            System.out.println("Fecha Actual: "+hoy);
+            System.out.println("La hora es:"+ hora);
+            //////////////////////////////////////////////////////////////////
             try (JsonGenerator gen = Json.createGenerator(swriter)) {
             gen.writeStartObject();
             gen.writeStartArray("listOfCalls");
             	//notificaciones ya cargadas (llamadas viejas)
             	while(rs2.next()) {
-            		old="true";
-            		if( rs2.getString(4)==null) {
+            		if(hoy.equals(rs2.getString(4))) {
+            			old="false";
+            		}
+            		else {
+            			old="true";
+            		}
+            		if( rs2.getString(5).equals(null) || rs2.getString(5).equals("") ) {
             			done="false";
             		}
-            		else done="true";
-            		//System.out.println("Resultado de la consulta de notificacion id:" + rs2.getInt(1)+ " nombre: "+rs2.getString(2)+rs2.getString(3)+" call: done: "+done+" active: "+active + " nota: "+rs2.getString(5)+"fecha: "+rs2.getString(4));
-            		gen.writeStartObject();
-	    	            gen.write("idStudent", rs2.getInt(1));
-	    	            gen.write("name", ""+rs2.getString(2)+  " " + rs2.getString(3));
-	    	            gen.writeStartObject("call");
-		    	            gen.write("done", done);
-		    	            gen.write("active", active);
-		    	            gen.write("note", ""+rs2.getString(5));
-		    	            gen.write("date", ""+rs2.getString(4));
-		    	            gen.write("old", old);
-		    	        gen.writeEnd();
-		    	    gen.writeEnd();
+            		else{
+            			done="true";
+            		}
+	            	if((old.equals("true") && done.equals("false")) || (old.equals("false"))){	
+	            		System.out.println("Resultado de la consulta de notificacion id:" + rs2.getInt(1)+ " nombre: "+rs2.getString(2)+rs2.getString(3)+" call: done: "+done+" active: "+active + " nota: "+rs2.getString(5)+"fecha: "+rs2.getString(4));
+            			gen.writeStartObject();
+		    	            gen.write("idStudent", rs2.getInt(1));
+		    	            gen.write("name", ""+rs2.getString(2)+  " " + rs2.getString(3));
+		    	            gen.writeStartObject("call");
+			    	            gen.write("done", done);
+			    	            gen.write("active", active);
+			    	            gen.write("note", ""+rs2.getString(5));
+			    	            gen.write("date", ""+rs2.getString(4));
+			    	            gen.write("old", old);
+			    	        gen.writeEnd();
+			    	    gen.writeEnd();
+	            	}
             	}
             	
             	//notificaciones para cargar (llamadas nuevas)
             	//System.out.println(rs.isBeforeFirst()+rs.getString(12));
-            	if(rs.getRow()==1 && rs.getInt(9)==1){ //no hay faltas
-            			System.out.println("recibo elerror" + rs.getString(9) );
-                }else { //si hay faltas
-                    System.out.println("Recibo info ");
-                    while(rs.next()){
-                    	//System.out.println("So" + rs.getInt(1));
-	            		old= "false";
-	            		List<Integer> diasQueViene = new ArrayList<Integer> ();
+            	if(hora>=19) { //PARA QUE SE EJECUTE DESPUES DE LAS 7 PM
+            		System.out.println("Entro a agregar nueva falta");
+	            	if(rs.getRow()==1 && rs.getInt(9)==1){ //no hay faltas
+	            			System.out.println("recibo elerror" + rs.getString(9) );
+	                }else { //si hay faltas
+	                    System.out.println("Recibo info ");
+	                    while(rs.next()){
+	                    	//System.out.println("So" + rs.getInt(1));
+		            		old= "false";
+		            		List<Integer> diasQueViene = new ArrayList<Integer> ();
+		
+		            		//System.out.println(rs.getString(1)+ " "+rs.getString(2)+ " "+rs.getInt(3)+ " "+rs.getString(4)+ " "+rs.getString(5)+ " "+rs.getString(6)+ " "+rs.getString(7)+ " "+rs.getString(8));
+		            		int diaAnterior=0;
+		            		int today=0;
+		            		
+		            		if(rs.getInt(8)==2) today=0;
+		            		if(rs.getInt(8)==4) today=1;
+		            		if(rs.getInt(8)==5) today=2;
+		            		if(rs.getInt(8)==7) today=3;
+		            		
+		            		// 2 lunes, 3 miercoles, 4 jueves, 6 sabado
+		            		arrayDays[0] = rs.getInt(4); //lunes
+		            		arrayDays[1] = rs.getInt(5); //miercoles
+		            		arrayDays[2] = rs.getInt(6); //jueves
+		            		arrayDays[3] = rs.getInt(7); //sabado
+		            		
+		            		int count=0;
+		            		
+		            		for(int i=0; i<4; i++) {
+		            			
+		            			if(today != i && arrayDays[i] == 1) {
+		            				//System.out.println("dia: " + arrayDays[i]+ i);
+		            				if(i == 0) diasQueViene.add(2);
+		            				if(i == 1) diasQueViene.add(4);
+		            				if(i == 2) diasQueViene.add(5);
+		            				if(i == 3) diasQueViene.add(7);
+		            			}
+		            		}
+		            		int size=diasQueViene.size();
+		            		
+		            		if(size == 0) {
+		            			//System.out.println("size" + diasQueViene.size());
+		                		//System.out.println("El dia inmediato anterior es: " + rs.getInt(8));
+		                		diaAnterior=rs.getInt(8);
+		
+		            		}else {
+		            			//System.out.println("size" + diasQueViene.size());
+		                		//System.out.println("El dia inmediato anterior es: " + diasQueViene.get(size-1));
+		                		diaAnterior=diasQueViene.get(size-1);
+		            		}
+		            		
+		            		//Ejecutando uno a uno los alumnos para ver si pertenecen a la lista dellamdas por realizar (es su segunda falta)
+		            		cStmt.setInt(1, diaAnterior); //dìa anterior
+		        		    cStmt.setInt(2, rs.getInt(3)); //id del alumno
+		        		    //System.out.println("holi:"+rs.getInt(3) + diaAnterior);
+		        		    cStmt.registerOutParameter(3, Types.INTEGER); // id regreso
+		        		    cStmt.registerOutParameter(4, Types.VARCHAR); // nombre
+		        		    cStmt.registerOutParameter(5, Types.VARCHAR); // apellido
+		        		    cStmt.registerOutParameter(6, Types.VARCHAR); // nota
+		        		    cStmt.registerOutParameter(7, Types.DATE);	  // fecha
+		        		    cStmt.registerOutParameter(8, Types.INTEGER); // estatus
+		        		    cStmt.registerOutParameter(9, Types.INTEGER); // activacion
+		        		    cStmt.registerOutParameter(10, Types.DATE);	  // fecha de ultima asistencia
+		        		    cStmt.registerOutParameter(11, Types.INTEGER); // ERROR
+		        		    cStmt.registerOutParameter(12, Types.VARCHAR); // MENSAJE DE ERROR
 	
-	            		//System.out.println(rs.getString(1)+ " "+rs.getString(2)+ " "+rs.getInt(3)+ " "+rs.getString(4)+ " "+rs.getString(5)+ " "+rs.getString(6)+ " "+rs.getString(7)+ " "+rs.getString(8));
-	            		int diaAnterior=0;
-	            		int today=0;
-	            		
-	            		if(rs.getInt(8)==2) today=0;
-	            		if(rs.getInt(8)==4) today=1;
-	            		if(rs.getInt(8)==5) today=2;
-	            		if(rs.getInt(8)==7) today=3;
-	            		
-	            		// 2 lunes, 3 miercoles, 4 jueves, 6 sabado
-	            		arrayDays[0] = rs.getInt(4); //lunes
-	            		arrayDays[1] = rs.getInt(5); //miercoles
-	            		arrayDays[2] = rs.getInt(6); //jueves
-	            		arrayDays[3] = rs.getInt(7); //sabado
-	            		
-	            		int count=0;
-	            		
-	            		for(int i=0; i<4; i++) {
-	            			
-	            			if(today != i && arrayDays[i] == 1) {
-	            				//System.out.println("dia: " + arrayDays[i]+ i);
-	            				if(i == 0) diasQueViene.add(2);
-	            				if(i == 1) diasQueViene.add(4);
-	            				if(i == 2) diasQueViene.add(5);
-	            				if(i == 3) diasQueViene.add(7);
-	            			}
-	            		}
-	            		int size=diasQueViene.size();
-	            		
-	            		if(size == 0) {
-	            			//System.out.println("size" + diasQueViene.size());
-	                		//System.out.println("El dia inmediato anterior es: " + rs.getInt(8));
-	                		diaAnterior=rs.getInt(8);
-	
-	            		}else {
-	            			//System.out.println("size" + diasQueViene.size());
-	                		//System.out.println("El dia inmediato anterior es: " + diasQueViene.get(size-1));
-	                		diaAnterior=diasQueViene.get(size-1);
-	            		}
-	            		
-	            		//Ejecutando uno a uno los alumnos para ver si pertenecen a la lista dellamdas por realizar (es su segunda falta)
-	            		cStmt.setInt(1, diaAnterior); //dìa anterior
-	        		    cStmt.setInt(2, rs.getInt(3)); //id del alumno
-	        		    //System.out.println("holi:"+rs.getInt(3) + diaAnterior);
-	        		    cStmt.registerOutParameter(3, Types.INTEGER); // id regreso
-	        		    cStmt.registerOutParameter(4, Types.VARCHAR); // nombre
-	        		    cStmt.registerOutParameter(5, Types.VARCHAR); // apellido
-	        		    cStmt.registerOutParameter(6, Types.VARCHAR); // nota
-	        		    cStmt.registerOutParameter(7, Types.DATE);	  // fecha
-	        		    cStmt.registerOutParameter(8, Types.INTEGER); // estatus
-	        		    cStmt.registerOutParameter(9, Types.INTEGER); // activacion
-	        		    cStmt.registerOutParameter(10, Types.DATE);	  // fecha de ultima asistencia
-	        		    cStmt.registerOutParameter(11, Types.INTEGER); // ERROR
-	        		    cStmt.registerOutParameter(12, Types.VARCHAR); // MENSAJE DE ERROR
-
-	        		    
-	        		    cStmt.execute();    
-
-	        		    System.out.println("idAlumno enviado: " +rs.getInt(3) + " idAlumno regresado: "+ cStmt.getInt(3));
-	        		    if (cStmt.getInt(11) == 0){
-	        		    	System.out.println("idAlumno: " + cStmt.getInt(3));
 		        		    
-		        		    /*alumn.setIdAlumno(Integer.toString(cStmt.getInt(3)));
-		        		    alumn.setNombre(cStmt.getString(4));
-		        		    alumn.setApellidoPaterno(cStmt.getString(5));*/
-		        		    nota=""+cStmt.getString(6);
-		        		    //System.out.println("nota: "+cStmt.getString(6));
-		        		    fechaLlamada=""+cStmt.getString(7);
-		        		    estatus=cStmt.getInt(8);
-		        		    activacion=cStmt.getInt(9);
-		        		    ultimaAsistencia=cStmt.getString(10);
-		        		    if(estatus == 1) done="true";	
-		        		    else done="false";
-		        		    if(activacion == 1) active="true";	
-		        		    else active="false";
-		        		    if(cStmt.getInt(11)==0 && cStmt.getInt(3)!=0){
-		        		    	System.out.println("Entro al los alumnos con dos faltas");
-		        		    	gen.writeStartObject();
-				    	            gen.write("idStudent", cStmt.getInt(3));
-				    	            gen.write("name", cStmt.getString(4) +  " " + cStmt.getString(5));
-				    	            gen.writeStartObject("call");
-					    	            gen.write("done", done);
-					    	            gen.write("active", active);
-					    	            gen.write("note", nota);
-					    	            gen.write("date", fechaLlamada);
-					    	            gen.write("old", old);
-					    	        gen.writeEnd();
-					    	     gen.writeEnd();
-		        		    }
-			    	     }
-	            	} 
-                }
+		        		    cStmt.execute();    
+	
+		        		    System.out.println("idAlumno enviado: " +rs.getInt(3) + " idAlumno regresado: "+ cStmt.getInt(3));
+		        		    if (cStmt.getInt(11) == 0){
+		        		    	System.out.println("idAlumno: " + cStmt.getInt(3));
+			        		    
+			        		    /*alumn.setIdAlumno(Integer.toString(cStmt.getInt(3)));
+			        		    alumn.setNombre(cStmt.getString(4));
+			        		    alumn.setApellidoPaterno(cStmt.getString(5));*/
+			        		    nota=""+cStmt.getString(6);
+			        		    //System.out.println("nota: "+cStmt.getString(6));
+			        		    fechaLlamada=""+cStmt.getString(7);
+			        		    estatus=cStmt.getInt(8);
+			        		    activacion=cStmt.getInt(9);
+			        		    ultimaAsistencia=cStmt.getString(10);
+			        		    if(estatus == 1) done="true";	
+			        		    else done="false";
+			        		    if(activacion == 1) active="true";	
+			        		    else active="false";
+			        		    if(cStmt.getInt(11)==0 && cStmt.getInt(3)!=0){
+			        		    	System.out.println("Entro al los alumnos con dos faltas");
+			        		    	gen.writeStartObject();
+					    	            gen.write("idStudent", cStmt.getInt(3));
+					    	            gen.write("name", cStmt.getString(4) +  " " + cStmt.getString(5));
+					    	            gen.writeStartObject("call");
+						    	            gen.write("done", done);
+						    	            gen.write("active", active);
+						    	            gen.write("note", nota);
+						    	            gen.write("date", fechaLlamada);
+						    	            gen.write("old", old);
+						    	        gen.writeEnd();
+						    	     gen.writeEnd();
+			        		    }
+				    	     }
+		            	} 
+	                }
+            	}
 /*
 	        		    //System.out.println("idAlumno: " + cStmt.getString(3));
 	        		    
