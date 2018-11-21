50bf917981543f249288f9055a425122a4b8c0c6
VanessaIPN <vanessamm.ipn@gmail.com>
2018-11-21 01:31:32 -0600

    intento de corregir

---
+++ pt1.pt2/src/main/java/classes/Registro.java
@@ -0,0 +1,287 @@
+package classes;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+
+import org.drools.core.WorkingMemory;
+
+public class Registro {
+	private int idRegistro;
+	private int idUsuario;
+	private int idAlumno;
+	private int idSet;
+	private String fecha;
+	private String tipo;
+	private int tiempo;
+	private int calificaciones[];
+	private String evaluacion;
+	private String accion;
+	private int numCien;
+	private int numNoventa;
+	private int numOchenta;
+	private int numSetenta;
+	private int numFlecha;
+	private int numTriangulo;
+	private Nivel nivel;
+	private double frec;
+	private String TipoFrec;
+	private int indice;
+	
+	
+	public int getIndice() {
+		return indice;
+	}
+	public void setIndice(int indice) {
+		this.indice = indice;
+	}
+	public void setAccion(String accion) {
+		this.accion = accion;
+	}
+	public String getAccion() {
+		return accion;
+	}
+	public double getFrec() {
+		return frec;
+	}
+	public void setFrec(float frec) {
+		this.frec = frec;
+	}
+	public Nivel getNivel() {
+		return nivel;
+	}
+	public void setNivel(Nivel nivel) {
+		this.nivel = nivel;
+	}
+	public int getNumCien() {
+		return numCien;
+	}
+	public void setNumCien(int numCien) {
+		this.numCien = numCien;
+	}
+	public int getNumNoventa() {
+		return numNoventa;
+	}
+	public void setNumNoventa(int numNoventa) {
+		this.numNoventa = numNoventa;
+	}
+	public int getNumOchenta() {
+		return numOchenta;
+	}
+	public void setNumOchenta(int numOchenta) {
+		this.numOchenta = numOchenta;
+	}
+	public int getNumSetenta() {
+		return numSetenta;
+	}
+	public void setNumSetenta(int numSetenta) {
+		this.numSetenta = numSetenta;
+	}
+	public int getNumFlecha() {
+		return numFlecha;
+	}
+	public void setNumFlecha(int numFlecha) {
+		this.numFlecha = numFlecha;
+	}
+	public int getNumTriangulo() {
+		return numTriangulo;
+	}
+	public void setNumTriangulo(int numTriangulo) {
+		this.numTriangulo = numTriangulo;
+	}
+	public String getFecha() {
+		return fecha;
+	}
+	public void setFecha(java.util.Date fecha) {
+		SimpleDateFormat formatF = new SimpleDateFormat("yyyy-MM-dd");
+		this.fecha = formatF.format(fecha);	
+	}
+	public int getIdRegistro() {
+		return idRegistro;
+	}
+	public void setIdRegistro(int idRegistro) {
+		this.idRegistro = idRegistro;
+	}
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public int getIdAlumno() {
+		return idAlumno;
+	}
+	public void setIdAlumno(int idAlumno) {
+		this.idAlumno = idAlumno;
+	}
+	public int getIdSet() {
+		return idSet;
+	}
+	public void setIdSet(int idSet) {
+		this.idSet = idSet;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public int getTiempo() {
+		return tiempo;
+	}
+	public void setTiempo(int tiempo) {
+		this.tiempo = tiempo;
+	}
+	/*public int getUno() {
+		return uno;
+	}
+	public void setUno(int uno) {
+		this.uno = uno;
+	}
+	public int getDos() {
+		return dos;
+	}
+	public void setDos(int dos) {
+		this.dos = dos;
+	}
+	public int getTres() {
+		return tres;
+	}
+	public void setTres(int tres) {
+		this.tres = tres;
+	}
+	public int getCuatro() {
+		return cuatro;
+	}
+	public void setCuatro(int cuatro) {
+		this.cuatro = cuatro;
+	}
+	public int getCinco() {
+		return cinco;
+	}
+	public void setCinco(int cinco) {
+		this.cinco = cinco;
+	}
+	public int getSeis() {
+		return seis;
+	}
+	public void setSeis(int seis) {
+		this.seis = seis;
+	}
+	public int getSiete() {
+		return siete;
+	}
+	public void setSiete(int siete) {
+		this.siete = siete;
+	}
+	public int getOcho() {
+		return ocho;
+	}
+	public void setOcho(int ocho) {
+		this.ocho = ocho;
+	}
+	public int getNueve() {
+		return nueve;
+	}
+	public void setNueve(int nueve) {
+		this.nueve = nueve;
+	}
+	public int getDiez() {
+		return diez;
+	}
+	public void setDiez(int diez) {
+		this.diez = diez;
+	}*/
+	public String getEvaluacion() {
+		return evaluacion;
+	}
+	public int[] getCalificaciones() {
+		return calificaciones;
+	}
+	public void setCalificaciones(int[] calificaciones) {
+		this.calificaciones = calificaciones;
+	}
+	public void setEvaluacion(String evaluacion) {
+		this.evaluacion = evaluacion;
+	}
+	public String getTipoFrec() {
+		return TipoFrec;
+	}
+	public void setTipoFrec(String tipoFrec) {
+		TipoFrec = tipoFrec;
+	}
+	//public int[] getCalificaciones() {
+		//return calificaciones;
+	//}
+	//public void setCalificaciones() {
+		//this.calificaciones = new int[]{this.uno, this.dos, this.tres, this.cuatro, this.cinco, this.seis, this.siete, this.ocho, this.nueve, this.diez};
+	//}
+	
+	
+	 public void setCantidadCalificaciones() {
+		 this.numCien=0;
+		 this.numNoventa=0;
+		 this.numOchenta=0;
+		 this.numSetenta=0;
+		 this.numFlecha=0;
+		 this.numTriangulo=0;
+		 //int[] calificaciones = new int[]{rs.uno, rs.dos, rs.tres, rs.cuatro, rs.cinco, rs.seis, rs.siete, rs.ocho, rs.nueve, rs.diez};
+		 for(int x=0; x<10;x++) {
+			 switch (this.calificaciones[x]){
+			 case 70:
+				 this.numSetenta=this.numSetenta+1;
+			 break;
+			 case 80:
+				 this.numOchenta=this.numOchenta+1;
+				 //System.out.println("numOchenta: " + r.numOchenta);
+			 break;
+			 case 90:
+				 this.numNoventa=this.numNoventa+1;
+			 break;
+			 case 100:
+				 this.numCien=this.numCien+1;
+			 break;
+			 case 110:
+				 this.numTriangulo=this.numTriangulo+1;
+			 break;
+			 case 120:
+				 this.numFlecha=this.numFlecha+1;
+			 break;
+			 
+				 
+			 }
+		 }
+		 //wm.insert(r);
+		 //wm.fireAllRules();
+		 //System.out.println(r.getEvaluacion());
+	 }
+	 
+	 public void setearValores(int idAlumno, String nivel, Connection conn) throws SQLException{
+		 	int idNivel=0;
+		 	String getQueryStatement = "select idNivel from Nivel where nombre='"+nivel+"';";
+			PreparedStatement prepareStat = conn.prepareStatement(getQueryStatement);
+			ResultSet rs = prepareStat.executeQuery();
+			while(rs.next()) {
+				idNivel=rs.getInt(1);
+			}
+		 	CallableStatement cS2 = conn.prepareCall("{call getLastFrecuencia(?, ?, ?, ?)}");
+			cS2.setInt(1, idAlumno);
+		 	cS2.setInt(2, idNivel);
+		 	cS2.registerOutParameter(3, Types.DOUBLE);
+		 	cS2.registerOutParameter(4, Types.VARCHAR);
+		 	cS2.execute();
+
+		 	this.frec=cS2.getDouble(3);
+		 	this.TipoFrec=cS2.getString(4);
+		 	
+	 }
+	
+	
+}
---
+++ pt1.pt2/src/main/java/classes/pruebaProg.java
@@ -0,0 +1,32 @@
+package classes;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.drools.compiler.compiler.DroolsParserException;
+import org.drools.core.WorkingMemory;
+import org.json.JSONObject;
+
+public class pruebaProg {
+	public static void main(String[] args) throws DroolsParserException, IOException, SQLException{
+		Auxiliar aux= new Auxiliar();
+		
+		//String fileRules1="../rules/proyeccionNivel.drl";
+		//String fileRules2="../rules/desempeño.drl";
+		String fileRules1="../rules/registro_progDiaria.drl";
+		String fileRules2="../rules/accion.drl";
+		String arrayJson=aux.crearJsonRegistro();
+		JSONObject obj = new JSONObject(arrayJson);
+		WorkingMemory wk1=aux.conexionDrools(fileRules1);
+		WorkingMemory wk2=aux.conexionDrools(fileRules2);
+		JSONObject results = obj.getJSONObject("resultsRegistro");
+		aux.executeProg(wk1,wk2,arrayJson);
+		//aux.executeProg(wk1, wk2, arrayJson);
+		//aux.executeFrecuencias(wk);
+		//aux.executeFrecInicial(wk, arrayJson);
+		
+		//aux.exFrecuencias(wk,2);
+
+	}
+}
+
